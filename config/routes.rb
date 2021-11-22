b2f4a95e45d3e39c0510d4f9667637fe514096aa
Jonjo <jgilbertons@gmail.com>
2021-11-22 20:33:43 -0300

    create simples index page

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
-  get 'pages/index'
-  get 'jobs/index'
   devise_for :headhunters
   devise_for :users
+  
+  resources :pages
+
+  root to: 'pages#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
