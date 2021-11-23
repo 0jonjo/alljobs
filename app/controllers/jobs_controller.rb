1f56f2065fbe89371b5d8c9b2fc0577a50fbe06a
Jonjo <jgilbertons@gmail.com>
2021-11-22 22:02:52 -0300

    update jobs controller functions

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -1,4 +1,51 @@
 class JobsController < ApplicationController
+  
+before_action :authenticate_headhunter!, except: [:index]
+  
   def index
+    @jobs = Job.all
   end
+
+  def new 
+    @job = Job.new
+  end
+
+  def create 
+    @job = Job.new(job_params)
+    if  @job.save
+      :new
+      redirect_to fallback_location: @jobs
+    end
+  end
+
+  def edit
+    @job = Job.find(params[:id])
+  end
+
+  def update
+    @job = Job.find(params[:id])
+
+    if  @job.update(job_params)
+      redirect_to @job
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @job = Job.find(params[:id])
+    @job.destroy
+
+    redirect_to root_path
+  end
+
+  def show
+    @job = Job.find(params[:id])
+  end
+
+  private
+  def job_params
+    params.require(:job).permit(:title, :description, :skills, :salary, :company, :level, :place, :date)
+  end
+
 end
