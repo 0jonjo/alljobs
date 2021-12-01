4c0ad473ed7f629550b685948320b234c27ae6ee
Jonjo <jgilbertons@gmail.com>
2021-11-30 21:48:03 -0300

    simple adjust profiles controller

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -24,7 +24,7 @@ class ProfilesController < ApplicationController
   end
 
   def update
-   @profile = Profile.find(params[:id])
+    @profile = Profile.find(params[:id])
 
     if @profile.update(profile_params)
       redirect_to @profile
