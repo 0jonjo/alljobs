d8239cf335f5c72908849ac571fa266d24f6fa26
Jonjo <jgilbertons@gmail.com>
2021-12-02 14:30:26 -0300

    add waden test helpers

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -31,6 +31,8 @@ rescue ActiveRecord::PendingMigrationError => e
   exit 1
 end
 RSpec.configure do |config|
+  config.include Warden::Test::Helpers
+
   config.before(type: :system) do
     driven_by :rack_test
   end
