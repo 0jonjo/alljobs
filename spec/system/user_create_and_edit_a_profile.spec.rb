9972ae6b915eb9ba8f6be357a1833634f6336bd1
Jonjo <jgilbertons@gmail.com>
2021-12-02 14:45:00 -0300

    update user create profile with devise capybara user

--- spec/system/user_create_and_edit_a_profile.spec.rb
+++ spec/system/user_create_and_edit_a_profile.spec.rb
@@ -26,7 +26,27 @@ describe 'Visit the homepage' do
         expect(page).to have_content 'José Teste da Silva'
         click_on 'Return to Home'
         expect(page).to have_content 'All Jobs'
+    end
 
+    it 'create and edit profile' do
+        
+        user = User.create!(:email => 'usuario@disco1995.com.br', :password => 'd2blackalien')
+        login_as(user, :scope => :user)
+        visit root_path
+
+        click_on 'Create/Edit Profile'
+        fill_in 'Name', with: 'José Teste da Silva'
+        fill_in 'Social name', with: 'Josefa Teste da Silva'
+        fill_in 'Birthdate', with: '21/03/1977'
+        fill_in 'Description', with: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam eget ligula eu lectus lobortis condimentum. Aliquam nonummy auctor massa. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Nulla at risus. Quisque purus magna, auctor et, sagittis ac, posuere eu, lectus. Nam mattis, felis ut adipiscing.'
+        fill_in 'Educacional background', with: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam eget ligula eu lectus lobortis condimentum. Aliquam nonummy auctor massa. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Nulla at risus. Quisque purus magna, auctor et, sagittis ac, posuere eu, lectus. Nam mattis, felis ut adipiscing.'
+        fill_in 'Experience', with: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam eget ligula eu lectus lobortis condimentum. Aliquam nonummy auctor massa. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Nulla at risus. Quisque purus magna, auctor et, sagittis ac, posuere eu, lectus. Nam mattis, felis ut adipiscing.'
+        click_on 'Create Profile'
+
+        expect(page).to have_content 'José Teste da Silva'
+        click_on 'Return to Home'
+        expect(page).to have_content 'All Jobs'
+        
         click_on 'Create/Edit Profile'
         fill_in 'Name', with: 'Josefino Teste da Silva'
 
