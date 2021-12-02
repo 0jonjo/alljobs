d9597c0e105b938e25bab1a038cabc0286eed8a8
Jonjo <jgilbertons@gmail.com>
2021-12-02 15:06:07 -0300

    adjust visitor visit homepage test

--- spec/system/visitor_visit_homepage_spec.rb
+++ spec/system/visitor_visit_homepage_spec.rb
@@ -3,10 +3,11 @@ require 'rails_helper'
 describe 'Visitor visit homepage' do
 
   it 'and view title' do
-    # Act
     visit root_path
-
-    # Assert
     expect(page).to have_content('All Jobs')
+    expect(page).to have_content('Login User')
+    expect(page).to have_content('New User')
+    expect(page).to have_content('Login Headhunter')
+    expect(page).to have_content('New Headhunter')
   end
 end
