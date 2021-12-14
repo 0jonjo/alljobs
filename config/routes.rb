c7f16e7ea7af4c27666b97bc5071dcb827ef2ddc
Jonjo <jgilbertons@gmail.com>
2021-12-14 07:32:54 -0300

    adjust routes resources comments

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,15 @@
 Rails.application.routes.draw do
   devise_for :headhunters, :users
 
-  resources :pages, :profiles, :applies
+  resources :pages, :applies
 
   resources :jobs do
     post :enroll
   end
- 
+
+  resources :profiles do
+    resources :comments
+  end
+
   root to: 'pages#index'
 end
