424973cda0a1fd441d7502fcf58c09d8d7939dcf
Jonjo <jgilbertons@gmail.com>
2021-12-13 21:55:53 -0300

    create test headhunter see a applies to a specific job

---
+++ spec/system/headhunter_see_a_applies_to_a_specific_job_spec.rb
@@ -0,0 +1,33 @@
+require 'rails_helper'
+
+describe 'Visit the homepage as Headhunter' do
+
+  it 'see applies to a specific job' do
+  
+    Job.new(title: 'Job Opening Test 123', description: 'Lorem ipsum dolor sit amet', 
+    skills: 'Nam mattis, felis ut adipiscing.', salary: '$99/m', company: 'Acme', level: 'Junior', 
+    place: 'Remote Job').save 
+
+    user = User.create!(:email => 'usuario@disco1995.com.br', :password => 'd2blackalien')
+    login_as(user, :scope => :user)
+    visit root_path
+
+    visit root_path
+    click_on 'Openings'
+    click_on 'Job Opening Test 123'
+    click_on 'Apply for this Job'
+    logout(:user)
+
+    headhunter = Headhunter.create!(:email => 'usuario@disco1995.com.br', :password => 'd2blackalien')
+    login_as(headhunter, :scope => :headhunter)
+    visit root_path
+
+    click_on 'Openings'
+    expect(page).to have_content('Job Opening Test 123')
+    click_on 'Job Opening Test 123'
+    expect(page).to have_content('All applies to this job')
+    click_on 'All applies to this job'
+    expect(page).to have_content('ID: 1') 
+    expect(page).to have_content('User E-mail: usuario@disco1995.com.br ')
+  end
+end
