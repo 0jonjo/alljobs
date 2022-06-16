ae24760592f8cde3f63346f2e32056c26e6b4fde
Jonjo <jgilbertons@gmail.com>
2022-06-16 16:01:07 -0300

    adjust signup headhunter and user tests

--- spec/system/signup_logins/signup_a_headhunter_spec.rb
+++ spec/system/signup_logins/signup_a_headhunter_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
 describe 'Signup a Headhunter' do
-  it 'with sucesso' do
+  it 'with sucess' do
     visit root_path
     click_on 'Login Headhunter'
     click_on 'Sign up'
@@ -13,5 +13,20 @@ describe 'Signup a Headhunter' do
     expect(current_path).to eq root_path
     expect(page).to have_content 'All Jobs'
     expect(page).to have_content 'You are admin@test.com'
+  end
+
+  it 'without sucess' do
+    visit root_path
+    click_on 'Login Headhunter'
+    click_on 'Sign up'
+    fill_in 'Email', with: ''
+    fill_in 'Password', with: ''
+    fill_in 'Password confirmation', with: ''
+    click_on 'Sign up'
+
+    expect(page).to have_content 'All Jobs'
+    expect(page).to have_content "Email can't be blank"
+    expect(page).to have_content "Password can't be blank"
+    expect(page).not_to have_content 'Logout'
   end
 end
--- spec/system/signup_logins/signup_a_user_spec.rb
+++ spec/system/signup_logins/signup_a_user_spec.rb
@@ -1,7 +1,6 @@
 require 'rails_helper'
 
 describe 'Signup a User' do
-
   it 'with sucess' do
     visit root_path
     click_on 'Login User'
@@ -14,5 +13,20 @@ describe 'Signup a User' do
     expect(current_path).to eq root_path
     expect(page).to have_content 'All Jobs'
     expect(page).to have_content 'You are user@test.com'
+  end
+
+  it 'without sucess' do
+    visit root_path
+    click_on 'Login User'
+    click_on 'Sign up'
+    fill_in 'Email', with: ''
+    fill_in 'Password', with: ''
+    fill_in 'Password confirmation', with: ''
+    click_on 'Sign up'
+
+    expect(page).to have_content 'All Jobs'
+    expect(page).to have_content "Email can't be blank"
+    expect(page).to have_content "Password can't be blank"
+    expect(page).not_to have_content 'Logout'
   end
 end
