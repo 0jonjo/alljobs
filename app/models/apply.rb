e96984f3edb01a73ef285c19cd9115b602f809d0
Jonjo <jgilbertons@gmail.com>
2022-06-17 17:41:53 -0300

    simply delete unecessary nested_attributtes

--- app/models/apply.rb
+++ app/models/apply.rb
@@ -1,8 +1,6 @@
 class Apply < ApplicationRecord
   belongs_to :job
   belongs_to :user
-  #accepts_nested_attributes_for :user
-  #accepts_nested_attributes_for :job
   
   def user_email
     @user = User.find(user_id)
--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,7 +1,5 @@
 class Comment < ApplicationRecord
   belongs_to :profile
   belongs_to :headhunter
-  accepts_nested_attributes_for :profile
-  accepts_nested_attributes_for :headhunter
   validates :body, :datetime, presence: true
 end
