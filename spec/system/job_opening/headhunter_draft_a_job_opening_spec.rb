9f799a06fe44abc33b4c47679637cc2bf72a9d74
Jonjo <jgilbertons@gmail.com>
2022-08-10 21:15:24 -0300

    simple adjust draft job test

--- spec/system/job_opening/headhunter_draft_a_job_opening_spec.rb
+++ spec/system/job_opening/headhunter_draft_a_job_opening_spec.rb
@@ -23,26 +23,4 @@ describe 'Headhunter draft a job opening using Active Job' do
     expect(page).to have_content 'There is no apply for this job.'
     expect(page).not_to have_content 'ID: 1'
   end
-
-  it 'with sucess - test 2' do
-    job = Job.create!(title: 'Job Opening Test', description: 'Lorem ipsum dolor sit amet', 
-                          skills: 'Nam mattis, felis ut adipiscing.', salary: '99',
-                          company: 'Acme', level: 'Junior', place: 'Remote Job',
-                          date: 1.month.from_now, job_status: :published)
-    user = User.create!(:email => 'user@test.com', :password => 'test123') 
-    apply = Apply.create!(job: job, user: user)
-    headhunter = Headhunter.create!(:email => 'admin@test.com', :password => 'test123')
-    login_as(headhunter, :scope => :headhunter)
-    visit job_path(job.id)
-    
-    click_on 'Change to Draft'
-    expect(page).to have_content 'ID: 1'
-    expect(page).to have_content 'Status Draft'
-    expect(page).not_to have_content 'Published'
-    
-    AppliesCleanupJob.new.perform(job.id)
-    visit job_path(job.id)
-    expect(page).to have_content 'There is no apply for this job.'
-    expect(page).not_to have_content 'ID: 1'
-  end
 end
