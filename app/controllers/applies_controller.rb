798e3fea01ec9d3bcdbf97f592805ff6758fde68
Jonjo <jgilbertons@gmail.com>
2023-01-08 09:22:13 -0300

    applies index only for user views

--- app/controllers/applies_controller.rb
+++ app/controllers/applies_controller.rb
@@ -6,7 +6,7 @@ class AppliesController < ApplicationController
   before_action :user_has_profile
   
   def index
-    user_signed_in? ? @applies = current_user.applies.page(params[:page]) : @applies = Apply.page(params[:page])
+    @applies = current_user.applies.page(params[:page])
   end
 
   def new 
