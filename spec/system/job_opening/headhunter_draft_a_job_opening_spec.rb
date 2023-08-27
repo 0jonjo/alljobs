eed284940b8e2bb0e3951951f407d4b66ff10c6e
Jonjo <jgilbertons@gmail.com>
2023-08-27 09:07:32 -0300

    [40] Refactoring views and job openings tests

--- spec/system/job_opening/headhunter_draft_a_job_opening_spec.rb
+++ spec/system/job_opening/headhunter_draft_a_job_opening_spec.rb
@@ -1,25 +1,30 @@
 require 'rails_helper'
 
-describe 'Headhunter draft a job opening using Active Job' do
-  it 'with sucess' do
-    ActiveJob::Base.queue_adapter = :delayed_job
-    job = Job.create!(title: 'Job Opening Test', description: 'Lorem ipsum dolor sit amet', 
-                          skills: 'Nam mattis, felis ut adipiscing.', salary: '99',
-                          company: 'Acme', level: 'Junior', place: 'Remote Job',
-                          date: 1.month.from_now, job_status: :published)
-    user = User.create!(:email => 'user@test.com', :password => 'test123') 
-    apply = Apply.create!(job: job, user: user)
-    headhunter = Headhunter.create!(:email => 'admin@test.com', :password => 'test123')
+describe 'Headhunter' do
+
+  let(:headhunter) { create(:headhunter) }
+  let!(:apply) { create(:apply) }
+
+  before do
     login_as(headhunter, :scope => :headhunter)
-    visit job_path(job.id)
+  end
 
-    click_on I18n.t('draft')
-    expect(page).to have_content('draft')
-    expect(page).not_to have_content('published')
+  context 'remove an apply to job opening using Active Job' do
 
-    Delayed::Worker.new.work_off
-    visit job_path(job.id)
+    it 'with sucess' do
+      ActiveJob::Base.queue_adapter = :delayed_job
 
-    expect(page).not_to have_content 'ID: 1'
+      visit job_path(apply.job_id)
+      click_on I18n.t('draft')
+
+      expect(page).to have_content('draft')
+      expect(page).not_to have_content('published')
+
+      Delayed::Worker.new.work_off
+      visit job_path(apply.job_id)
+
+      expect(page).not_to have_link(apply.user.email)
+      expect(page).not_to have_link(apply.id.to_s)
+    end
   end
 end
