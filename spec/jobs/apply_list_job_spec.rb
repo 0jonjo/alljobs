1e1b7505ad2167eea21e283c1bc0962219941969
Jonjo <jgilbertons@gmail.com>
2023-08-27 16:05:00 -0300

    [46] Adjust job tests

--- spec/jobs/apply_list_job_spec.rb
+++ spec/jobs/apply_list_job_spec.rb
@@ -4,19 +4,15 @@ RSpec.describe ApplyListJob, type: :job do
   describe "Clean applies to a job opening" do
     it 'with sucess' do
       ActiveJob::Base.queue_adapter = :test
-      expect(Delayed::Job.count).to eq 0 
-       job_to_list = Job.create!(title: 'Job Opening Test', description: 'Lorem ipsum', 
-                         skills: 'Nam mattis', salary: '99',
-                         company: 'Acme', level: 'Junior', place: 'Remote',
-                         date: 1.month.from_now, job_status: :published)
-       2.times do |n|
-         user = User.create!(:email => "user#{n}@test.com", :password => 'test123')
-         apply = Apply.create!(:job => job_to_list, :user => user) 
-       end
-    
+      expect(Delayed::Job.count).to eq 0
+
+      job_to_list = create(:job)
+       create(:apply, job: job_to_list)
+       create(:apply, job: job_to_list)
+
        ApplyListJob.perform_now(job_to_list)
-   
+
        expect(ApplyCleanupJob).to(have_been_enqueued.at_least(:twice))
-     end 
-   end 
+     end
+   end
 end
