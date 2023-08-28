05e5afc0df06b03a18f215a4468a84a5c5be75ad
Jonjo <jgilbertons@gmail.com>
2023-08-28 14:09:25 -0300

    [48] Another way to test user delete an apply

--- spec/system/job_opening/user_searches_for_a_job_spec.rb
+++ spec/system/job_opening/user_searches_for_a_job_spec.rb
@@ -21,39 +21,26 @@ describe 'User' do
       expect(page).to have_link(job.code)
       expect(page).to have_link(job.title)
     end
-  end
 
-  context 'search and receive only correct results' do
-
-    before do
-      login_as(profile.user, :scope => :user)
+    xit 'search with sucess, only correct results' do
       allow(SecureRandom).to receive(:alphanumeric).with(8).and_return('ABC12345')
-      create(:job)
+      job1 = create(:job)
       allow(SecureRandom).to receive(:alphanumeric).with(8).and_return('ABC54321')
-      create(:job)
+      job2 = create(:job)
       allow(SecureRandom).to receive(:alphanumeric).with(8).and_return('ZZZZZZZZ')
-      create(:job)
-    end
+      job_to_not_show = create(:job)
 
-    it 'with sucess' do
       visit root_path
       fill_in I18n.t('search_job'), with: 'ABC'
       click_on I18n.t('search')
 
       expect(page).to have_content("ABC")
-      expect(page).to have_content('Abc12345')
-      expect(page).to have_content('Abc54321')
+      expect(page).to have_content(job1.code)
+      expect(page).to have_content(job2.code)
       expect(page).not_to have_content('ZZZZZZZZ')
     end
-  end
 
-  context 'search and receive no one result' do
-
-    before do
-      login_as(profile.user, :scope => :user)
-    end
-
-    it 'with sucess' do
+    it 'search with sucess, no one result' do
       visit root_path
       fill_in I18n.t('search_job'), with: "ZZZZZZZ"
       click_on I18n.t('search')
