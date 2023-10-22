9946370253c3209ca241923090c2760b9f478fa1
Joao Gilberto Saraiva <jgilbertons@gmail.com>
2023-10-22 16:21:54 -0300

    refactor: change proposal comments to be polymorphic

---
+++ db/migrate/20231022190716_proposal_comment_author_be_polimorphic.rb
@@ -0,0 +1,6 @@
+class ProposalCommentAuthorBePolimorphic < ActiveRecord::Migration[7.1]
+  def change
+    remove_column :proposal_comments, :author
+    add_reference :proposal_comments, :author, polymorphic: true, index: true
+  end
+end
