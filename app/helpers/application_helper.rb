232f62cc58a72f49f26d964fb2ba8887207e61c3
Joao Gilberto Saraiva <jgilbertons@gmail.com>
2023-12-16 15:03:46 -0300

    refactor: job proposal using helpers

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,4 +1,11 @@
 # frozen_string_literal: true
 
 module ApplicationHelper
+  def format_datetime(datetime)
+    datetime.to_formatted_s(:long)
+  end
+
+  def format_date(date)
+    date.strftime('%d/%m/%Y')
+  end
 end
