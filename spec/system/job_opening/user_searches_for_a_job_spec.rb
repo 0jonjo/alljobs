71de355e5660e9357d7dd1fa562c6d4e63656aad
João Gilberto Saraiva <jgilbertons@gmail.com>
2024-05-05 09:15:42 -0300

    104 Remove cp from ruby.yml workflow (#106)
    * Remove cp from ruby.yml workflow
    
    * change workflow of tests to production
    
    * return to rails env test in pipeline
    
    * remove database.ci and return production configs to database.yml
    
    * Reorder process in ruby.yml
    
    * Rollback setup-ruby@v1
    
    * Update codeclimate-action to v6.0.0
    
    * remove problematic and not useful test
    
    * change postgres:12 to alpine version in ruby.yml

--- spec/system/job_opening/user_searches_for_a_job_spec.rb
+++ spec/system/job_opening/user_searches_for_a_job_spec.rb
@@ -22,24 +22,6 @@ describe 'User' do
       expect(page).to have_link(job.title)
     end
 
-    xit 'search with sucess, only correct results' do
-      allow(SecureRandom).to receive(:alphanumeric).with(8).and_return('ABC12345')
-      job1 = create(:job)
-      allow(SecureRandom).to receive(:alphanumeric).with(8).and_return('ABC54321')
-      job2 = create(:job)
-      allow(SecureRandom).to receive(:alphanumeric).with(8).and_return('ZZZZZZZZ')
-      create(:job)
-
-      visit root_path
-      fill_in I18n.t('search_job'), with: 'ABC'
-      click_on I18n.t('search')
-
-      expect(page).to have_content('ABC')
-      expect(page).to have_content(job1.code)
-      expect(page).to have_content(job2.code)
-      expect(page).not_to have_content('ZZZZZZZZ')
-    end
-
     it 'search with sucess, no one result' do
       visit root_path
       fill_in I18n.t('search_job'), with: 'ZZZZZZZ'
