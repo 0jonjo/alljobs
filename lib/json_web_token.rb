57ad0caabfe5e51c26e2a6ad8de1908781c72379
Joao Gilberto Saraiva <jgilbertons@gmail.com>
2024-05-19 18:46:35 -0300

    add json web token coder decoder

---
+++ lib/json_web_token.rb
@@ -0,0 +1,17 @@
+# frozen_string_literal: true
+
+class JsonWebToken
+  class << self
+    def encode(payload, exp = 24.hours.from_now)
+      payload[:exp] = exp.to_i
+      JWT.encode(payload, ENV['SECRET_KEY'])
+    end
+
+    def decode(token)
+      body = JWT.decode(token, ENV['SECRET_KEY'])[0]
+      HashWithIndifferentAccess.new body
+    rescue StandardError
+      nil
+    end
+  end
+end
