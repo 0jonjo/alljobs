7544dfb039ccd641b835f3b46cd7fa21ea8e02f0
Joao Gilberto Saraiva <jgilbertons@gmail.com>
2024-05-31 09:46:58 -0300

    Adjust secret keys and JsonWebToken

--- lib/json_web_token.rb
+++ lib/json_web_token.rb
@@ -4,11 +4,11 @@ class JsonWebToken
   class << self
     def encode(payload, exp = 24.hours.from_now)
       payload[:exp] = exp.to_i
-      JWT.encode(payload, ENV['SECRET_KEY'])
+      JWT.encode(payload, Rails.application.credentials.secret_key_base)
     end
 
     def decode(token)
-      body = JWT.decode(token, nil, false)[0]
+      body = JWT.decode(token, Rails.application.credentials.secret_key_base)[0]
       HashWithIndifferentAccess.new body
     rescue StandardError
       nil
