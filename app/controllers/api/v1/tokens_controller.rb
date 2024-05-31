9c0bb115886855b0f7ce814ee5c7bc31cd54cc84
Joao Gilberto Saraiva <jgilbertons@gmail.com>
2024-05-31 11:04:03 -0300

    rubocop changes

--- app/controllers/api/v1/tokens_controller.rb
+++ app/controllers/api/v1/tokens_controller.rb
@@ -3,11 +3,9 @@
 module Api
   module V1
     class TokensController < Api::V1::ApiController
-
       def auth_user
         auth = Authenticate.new(params[:email], params[:password]).call_user
         render status: :ok, json: { token: auth }
-
       rescue Authenticate::AuthenticationError => e
         render json: { error: e.message }, status: :not_found
       end
@@ -15,7 +13,6 @@ module Api
       def auth_headhunter
         auth = Authenticate.new(params[:email], params[:password]).call_headhunter
         render status: :ok, json: { token: auth }
-
       rescue Authenticate::AuthenticationError => e
         render json: { error: e.message }, status: :not_found
       end
