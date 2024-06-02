3abe3a33381e97026b7dc499560d1933e2359d64
Joao Gilberto Saraiva <jgilbertons@gmail.com>
2024-06-02 10:09:19 -0300

    add logs to api to obtain token

--- lib/json_web_token.rb
+++ lib/json_web_token.rb
@@ -8,8 +8,7 @@ class JsonWebToken
     end
 
     def decode(token)
-      body = JWT.decode(token, Rails.application.credentials.secret_key_base)[0]
-      HashWithIndifferentAccess.new body
+      JWT.decode(token, Rails.application.credentials.secret_key_base)
     rescue StandardError
       nil
     end
