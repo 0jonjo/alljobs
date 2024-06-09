9baea2bdafd9362e1c6b97ee2d3084e28e03e1da
João Gilberto Saraiva <jgilbertons@gmail.com>
2024-06-09 14:30:25 -0300

    129 remove old configs and files of database and jwt (#130)
    * remove old stuff of api requests
    
    * refactor JWT controllers and models
    
    * remove 5439 port database config

--- app/controllers/api/v1/tokens_controller.rb
+++ app/controllers/api/v1/tokens_controller.rb
@@ -4,24 +4,31 @@ module Api
   module V1
     class TokensController < Api::V1::ApiController
       def auth_user
-        auth = Authenticate.new(params[:email], params[:password]).call_user
-        log_auth_request_success(User.model_name.human, params[:email])
-        render status: :ok, json: { token: auth }
-      rescue ActiveRecord::RecordNotFound
-        log_auth_request_fail(User.model_name.human, params[:email])
-        render json: { error: incorrect_data_user }, status: :not_found
+        authenticate("User")
       end
 
       def auth_headhunter
-        auth = Authenticate.new(params[:email], params[:password]).call_headhunter
-        log_auth_request_success(Headhunter.model_name.human, params[:email])
-        render status: :ok, json: { token: auth }
-      rescue ActiveRecord::RecordNotFound
-        log_auth_request_fail(Headhunter.model_name.human, params[:email])
-        render json: { error: incorrect_data_headhunter }, status: :not_found
+        authenticate("Headhunter")
       end
 
       private
+
+      def authenticate(user_type)
+        auth = Authenticate.new(params[:email], params[:password], user_type).call
+        auth_user_type(user_type, auth)
+      rescue ActiveRecord::RecordNotFound
+        log_and_return_error(user_type)
+      end
+
+      def auth_user_type(user_type, auth)
+        log_auth_request_success(user_type.constantize.model_name.human, params[:email])
+        render json: { token: auth }, status: :ok
+      end
+
+      def log_and_return_error(user_type)
+        log_auth_request_fail(user_type.constantize.model_name.human, params[:email])
+        render json: { error: incorrect_data(user_type.constantize.model_name.human) }, status: :not_found
+      end
 
       def log_auth_request_success(class_name, email)
         logger.info(I18n.t("auth.log.auth_request_success", class_name: class_name, email: email))
@@ -31,12 +38,8 @@ module Api
         logger.info(I18n.t("auth.log.auth_request_fail", class_name: class_name, email: email))
       end
 
-      def incorrect_data_user
-        I18n.t("auth.incorrect_data", class_name: User.model_name.human)
-      end
-
-      def incorrect_data_headhunter
-        I18n.t("auth.incorrect_data", class_name: Headhunter.model_name.human)
+      def incorrect_data(class_name)
+        I18n.t("auth.incorrect_data", class_name: class_name)
       end
     end
   end
