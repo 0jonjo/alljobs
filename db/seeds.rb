bf53b73b4e4c1c6a9fc086eb5d65d8ba3cbc2b57
Joao Gilberto Saraiva <jgilbertons@gmail.com>
2025-01-25 11:15:28 -0300

    [152] Improve seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,43 +1,85 @@
 # frozen_string_literal: true
 
 # Default Headhunter
-headhunter = Headhunter.create(email: 'headhunter@test.com',
-                               password: 'test123')
+password = 'test123'
+headhunter = Headhunter.create(email: 'headhunter@test.com', password: password)
+
 # Default Country
 country = Country.create(acronym: 'US', name: 'United States')
 
 # Default User and Profile
-user = User.create(email: 'user@test.com',
-                   password: 'test123')
-profile = Profile.create!(name: 'Tester', social_name: 'Tester Social Name',
-                          birthdate: '21/03/1977', description: 'Nice person', educacional_background: 'High School',
-                          experience: 'Dancer long time ago', user_id: user.id,
-                          country_id: country.id, city: 'Test City')
+user = User.create(email: 'user@test.com', password: password)
+description = 'Description'
+profile = Profile.create!(
+  name: 'Tester',
+  social_name: 'Tester Social Name',
+  birthdate: '21/03/1977',
+  description:,
+  educacional_background: 'Educational Background',
+  experience: 'Experience',
+  user_id: user.id,
+  country_id: country.id,
+  city: 'City'
+)
+
 # Default Comment on Profile
-Comment.create(body: 'Seed comment', profile_id: profile.id,
-               headhunter_id: headhunter.id)
+comment_body = 'Just a text'
+Comment.create(
+  body: comment_body,
+  profile_id: profile.id,
+  headhunter_id: headhunter.id
+)
+
 # Default Company
-company = Company.create(name: 'Acme', description: 'Seeds Company',
-                         website: 'www.test.com', email: 'acme@test.com',
-                         phone: '999999999')
+company = Company.create(
+  name: 'Company',
+  description:,
+  website: 'www.test.com',
+  email: 'company@test.com',
+  phone: '999999999'
+)
+
 # Default Job
-job = Job.create(title: 'Test 123', description: 'Lorem ipsum dolor sit amet',
-                 skills: 'Nam mattis, felis ut adipiscing.', salary: 99, level: :junior,
-                 company_id: company.id, country_id: country.id, city: 'Test City', date: 10.years.from_now)
+salary = 99
+job = Job.create(
+  title: 'Title',
+  description:,
+  skills: 'Skills',
+  salary:,
+  level: :junior,
+  company_id: company.id,
+  country_id: country.id,
+  city: 'City',
+  date: 10.years.from_now
+)
+
 # Default Apply
-apply = Apply.create(job:, user:)
+apply = Apply.create(job: job, user: user)
 
 # Default Star
 Star.create(headhunter_id: headhunter.id, apply_id: apply.id)
 
 # Default Feedback Apply
-FeedbackApply.create(headhunter_id: headhunter.id, apply_id: apply.id, body: 'Just a text Feedback')
+FeedbackApply.create(
+  headhunter_id: headhunter.id,
+  apply_id: apply.id,
+  body: comment_body
+)
 
 # Default Proposal
-proposal = Proposal.create(apply:, salary: 999, benefits: 'some benefits',
-                           expectations: 'some expectations', expected_start: 1.month.from_now,
-                           user_accepted: true)
+proposal = Proposal.create(
+  apply: apply,
+  salary:,
+  benefits: 'Benefits',
+  expectations: 'Expectations',
+  expected_start: 1.month.from_now,
+  user_accepted: true
+)
 
 # Default Proposal Comment
-ProposalComment.create(proposal_id: proposal.id, author_id: headhunter.id, author_type: headhunter.class,
-                       body: 'Just a test comment')
+ProposalComment.create(
+  proposal: proposal,
+  author_id: headhunter.id,
+  author_type: headhunter.class,
+  body: comment_body
+)
