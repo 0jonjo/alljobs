89f8295b75637a867f04910596b258768ed0655c
Joao Gilberto Saraiva <jgilbertons@gmail.com>
2025-01-26 20:51:46 -0300

    [154] Create methods to check if user is owner of the resource

--- app/controllers/concerns/authenticable.rb
+++ app/controllers/concerns/authenticable.rb
@@ -20,6 +20,28 @@ module Authenticable
   def current_headhunter_id
     body = JsonWebToken.decode(@token)
 
-    body[0]['headhunter_id'] if body[0]['headhunter_id']
+    body[0]['headhunter_id'] if body && body[0] && body[0]['headhunter_id']
+  end
+
+  def current_user_id
+    body = JsonWebToken.decode(@token)
+
+    body[0]['user_id'] if body && body[0] && body[0]['user_id']
+  end
+
+  def not_headhunter
+    render_unauthorized unless current_headhunter_id
+  end
+
+  def not_owner
+    check_authorized(@profile.user_id)
+  end
+
+  def not_owner_params
+    check_authorized(params[:profile][:user_id])
+  end
+
+  def check_authorized(user_id)
+    render_unauthorized unless current_headhunter_id || (current_user_id && current_user_id == user_id)
   end
 end
